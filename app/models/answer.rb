5d35a250af756f370151184330f367a84109b9a9
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-13 19:34:08 +0530

    validation on question, answer, tag

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,6 @@
 class Answer < ApplicationRecord
+  validates :content, presence: true, length: { minimum: 10, maximum: 30_000 }
+
   belongs_to :user
   belongs_to :question
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ApplicationRecord
+  validates :heading, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 30_000 }
+
   belongs_to :user
 
   has_many :answers
