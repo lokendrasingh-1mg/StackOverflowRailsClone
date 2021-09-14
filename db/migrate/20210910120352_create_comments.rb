0bf1a6d7192677649a0fe978a713919ba6e03575
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-14 13:33:36 +0530

    reference in beginning

--- db/migrate/20210910120352_create_comments.rb
+++ db/migrate/20210910120352_create_comments.rb
@@ -2,9 +2,9 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.references :user
+      t.references :commentable, polymorphic: true
       t.string :content
       t.integer :votes, default: 0
-      t.references :commentable, polymorphic: true
       t.datetime :deleted_at, index: true
 
       t.timestamps
