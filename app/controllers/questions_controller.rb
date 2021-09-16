2bd55d7cb1482544c4b961ca875ef3a169601c42
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-16 22:27:28 +0530

    question-controller: add valid_action method for CRUD

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,66 +1,82 @@
 class QuestionsController < ApplicationController
-  before_action :populate_user, only: %i[create update destroy]
-  before_action :populate_question, only: %i[show update destroy]
-  before_action :authenticate_user, only: %i[update destroy]
+  before_action :validate_actions, only: %i[create show update destroy]
 
   def index
-    @questions = Question.includes(:answers)
+    @questions = Question.includes(:answers, :comments)
 
     render json: @questions
   end
 
-  def new
-  end
-
   def create
-    @question = @user.questions.create!(create_params)
+    @question = user.questions.create!(params_attributes)
 
     render json: @question
   end
 
   def show
-    render json: @question
-  end
-
-  def edit
+    render json: question
   end
 
   # TODO: enforce user authorization
   def update
-    @question.update!(create_params)
+    redirect_to root_path unless valid_user?
+    question.update!(params_attributes)
 
-    render json: @question
+    render json: question
   end
 
   # TODO: mark user as anonymous instead of deleting it
   # Maybe update the user to -1: anonymous user
   # or use paranoid to retrieve all questions
   def destroy
-    @question.destroy
+    redirect_to root_path unless valid_user?
+    question.destroy
 
     render json: { message: 'Delete Successful' }
   end
 
   private
 
-  def create_params
+  def validate_actions
+    action = params[:action]
+    send("valid_#{action}")
+  end
+
+  def valid_create
     param! :heading, String, required: true, message: 'Question heading not specified'
     param! :description, String, required: true, message: 'Question description not specified'
     param! :user_id, Integer, required: true
+  end
+
+  def valid_show
+    param! :id, Integer, required: true
+  end
+
+  def valid_update
+    param! :id, Integer, required: true
+    param! :heading, String, required: true, message: 'Question heading not specified'
+    param! :description, String, required: true, message: 'Question description not specified'
+    param! :user_id, Integer, required: true
+  end
+
+  def valid_destroy
+    param! :id, Integer, required: true
+    param! :user_id, Integer, required: true
+  end
+
+  def params_attributes
     params.permit(:heading, :description)
   end
 
-  def populate_user
-    param! :user_id, Integer, required: true
-    @user = User.find(params[:user_id])
+  def user
+    @user ||= User.find(params[:user_id])
   end
 
-  def populate_question
-    param! :id, Integer, required: true
-    @question = Question.find(params[:id])
+  def question
+    @question ||= Question.find(params[:id])
   end
 
-  def authenticate_user
-    redirect_to root_path unless @question.user_id == @user.id
+  def valid_user?
+    question.user_id == user.id
   end
 end
--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -2,4 +2,5 @@ class QuestionSerializer < ActiveModel::Serializer
   attributes :id, :heading, :description, :votes, :user_id
 
   has_many :answers
+  has_many :comments
 end
