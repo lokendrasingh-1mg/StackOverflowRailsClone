c04c0f58bdb12e9c20fa8e365a72dee750e4d2a8
Lokendra Singh <lokendra.singh@1mg.com>
2021-09-21 19:17:09 +0530

    initialize sidekiq

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,14 @@ module StackOverflowRailsClone
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
+    config.active_job.queue_adapter = :sidekiq
+
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
   end
 end
